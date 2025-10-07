48862831175e12591e52724bcf0e25858ca59431
realishaanlohani <ishaanlohani@gmail.com>
2025-10-07 23:36:39 +0530

    Made a numpy edit

---
+++ toctactoe.py
@@ -0,0 +1,4 @@
+import numpy as n
+print("How it works> \n You choose whether to go first or second \n Then you pick a number based on the diagram shown and then yay you play tictactoe, how nice")
+gameboard = n.array([['', '', ''], ['', '', ''], ['', '', '']])
+print(gameboard)
