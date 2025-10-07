addd36eab3aa12c6993a565e276c715ffb81c688
realishaanlohani <ishaanlohani@gmail.com>
2025-10-07 23:52:48 +0530

    added more files:

--- toctactoe.py
+++ toctactoe.py
@@ -1,4 +1,15 @@
 import numpy as n
 print("How it works> \n You choose whether to go first or second \n Then you pick a number based on the diagram shown and then yay you play tictactoe, how nice")
-gameboard = n.array([['', '', ''], ['', '', ''], ['', '', '']])
+gameboard = n.array([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
 print(gameboard)
+def first():
+    
+def second():
+    pass
+def choice():
+    a = input("Wheather you want to go first or second (f/s): ")
+    if a == 'f':
+        first()
+
+    elif a == 's':
+        second()
