4343d8e5ef7be8876a99a925ace903a5dc1a6c97
realishaanlohani <ishaanlohani@gmail.com>
2025-10-07 23:58:27 +0530

    updated

--- toctactoe.py
+++ toctactoe.py
@@ -1,9 +1,15 @@
 import numpy as n
 print("How it works> \n You choose whether to go first or second \n Then you pick a number based on the diagram shown and then yay you play tictactoe, how nice")
 gameboard = n.array([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
+
+pfield = n.array([['', '', ''], ['', '', ''], ['','','']])
+print(pfield)
 print(gameboard)
+def win():
+    # if []
+    pass
 def first():
-    
+    pass
 def second():
     pass
 def choice():
